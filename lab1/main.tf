05d7c72e55fa85b5d85140d0e8ef0d8198e51b50
Tam Nguyen <tamnct1999@gmail.com>
2025-10-04 19:40:27 +0700

    init project

---
+++ lab1/main.tf
@@ -0,0 +1,54 @@
+
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_key_pair" "tamnc-keypair" {
+  key_name   = "tamnc-keypair_name"
+  public_key = file("/Users/tamnc/Documents/keypair/terraform/tamnc-key.pub")
+}
+
+resource "aws_instance" "demo-instance" {
+  ami           = "ami-0453ec754f44f9a4a"
+  instance_type = "t3.micro"
+  key_name      = aws_key_pair.tamnc-keypair.key_name
+  tags = {
+    Name = "terraform-lab1"
+  }
+  vpc_security_group_ids = [aws_security_group.test-security-group.id]
+}
+
+resource "aws_security_group" "test-security-group" {
+  name        = "test-security-group-name"
+  description = "allow access"
+  vpc_id      = "vpc-0f81ea992b18513f7"
+  ingress {
+    description = "HTTP"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description = "HTTPS"
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    description      = "for all outgoing traffics"
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+
+  }
+
+  tags = {
+    Name = "terraform-lab1"
+  }
+}
